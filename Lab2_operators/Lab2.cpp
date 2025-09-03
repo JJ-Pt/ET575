c76eefed658be03c399ce660ada1b9bf92a4ecb4
JJ-Pt <jjpenetrate@gmail.com>
2025-09-03 19:13:43 +0000

    1

--- Lab2_operators/Lab2.cpp
+++ Lab2_operators/Lab2.cpp
@@ -27,7 +27,7 @@ int main(){
     // print results
     cout<<"The height of a free fall obejct at " << time << " seconds is " << height<< " meters" <<endl;
 
-    cout<<"ln----- example 2: assignment operator -----" << endl;
+    cout<<"\n----- example 2: assignment operator -----" << endl;
     int number1 = 10;
     int number2 = 3;
     cout<<"original number1 = "<< number1<<endl;
@@ -36,6 +36,25 @@ int main(){
     cout<<"updated number2 = " <<number2<<endl;
     number2 --;
     cout<<"updated number2 = " << number2 << endl;
+    number2 += 5;
+    cout<<"updated number2  by 5 = " << number2 << endl;
+    number1 *=2 ;
+    cout<<"updated number1 using *=2 -->  " <<number1<<endl;
+
+    cout<<"The remainder of  " << number1 <<" and " <<number2<<" is "<<number1%number2<<endl;
+    cout<<"number1 = "<<number1<<endl;
+    cout<<"number2 = "<<number2<<endl;
+
+    number1 % number2;
+    cout<<"after %=, number1 = " <<number1<<endl;
+    cout<<"after %=, number2 = " <<number2<<endl;
+
+    cout<<"\n----- example 3: comparison operator -----" << endl;
+    cout<<"is number1 equal to number2? " << (number1 == number2) << endl;
+    cout<<"is number1 greater than number2? " << (number1 > number2) << endl;
+    cout << "is number1 less than number2? " << (number1 < number2) << endl;
+    cout << "is number1 not equal to number2? " << (number1 != number2)<<endl;
+
 
     cout<<"\n----- exercise:  Simple Calculator and Logical Operations -----" << endl;
     int a;
@@ -45,10 +64,14 @@ int main(){
     cout << "Enter the second number(b): ";
     cin >> b;
 
+
+    //increase a by 10
     a += 10;
+    // decreace b by 5
     b -= 5;
 
-    cout << a;
+    cout << "Update A value = " << a;
+    cout << "Update B value = " << b;
 
     return 0;
 }
