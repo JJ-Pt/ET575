126ddf08214095608842a923e081da28b3fd2cfd
JJ-Pt <jjpenetrate@gmail.com>
2025-09-03 18:18:56 +0000

    lab

--- lab1_Li.cpp
+++ lab1_Li.cpp
@@ -4,42 +4,49 @@ Aug 27, 2025
 Lab 1, iostream
 */
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() 
 {
-    cout << "-----Exmaple 1: cout data-----" << endl;
+    cout << "-----Example 1: cout data-----" << endl;
     cout << "This is a C++ code";
-    // varibles = termporary stroge of data
+    // variables = temporary storage of data
     
-    // inclass exmaple
+    // inclass example
     string username = "Perterpen123";
     cout << "\nWelcome to C++ " << username << endl;
     
-    cout << "-----Exmaple 2: cin data-----" << endl;
+    cout << "-----Example 2: cin data-----" << endl;
     cout << "Enter a new username: ";
     cin >> username;
     cout << "The user is " << username << endl;
     
     
-    cout << "-----Excerise-----" << endl;
+    cout << "-----Exercise-----" << endl;
     string country;
     char gender;
     int zipcode;
     double rate;
 
-    cout << "Enter a Country: ";
+    cout << "Enter a country: ";
     cin >> country;
 
-    cout << "Selected Gender (f for female, m for male, and o for others): ";
+    cout << "Enter a gender (f for female, m for male, and o for others): ";
     cin >> gender;
 
-    cout << "Enter a Zip code: ";
+    cout << "Enter a zip code: ";
     cin >> zipcode;
 
-    cout << "Tax Rate: ";
+    cout << "Enter a tax rate: ";
     cin >> rate;
 
+    // Display all collected data with proper alignment using \t
+    cout << "\n-----Results-----" << endl;
+    cout << "Entered country:\t\t" << country << endl;
+    cout << "Selected gender:\t\t" << gender << endl;
+    cout << "Zip code:\t\t\t" << zipcode << endl;
+    cout << "Tax rate:\t\t\t" << rate << endl;
     
     return 0;
 }
