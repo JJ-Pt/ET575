ab3c8c227ba69484cfef2639d2603d16e9127620
JJ-Pt <jjpenetrate@gmail.com>
2025-09-03 18:50:26 +0000

    lab3

--- Lab2_operators/Lab2.cpp
+++ Lab2_operators/Lab2.cpp
@@ -10,8 +10,30 @@ using namespace std;
 
 int main(){
     cout<<"\n----- example 1: arithmetic operator -----" << endl;
-    
+    // set number format
+    cout.setf(ios::fixed);
+    cout.precision(3);
+    // calculate the height of a free falling object
+    // free_fall = o.5 gravity*time*time
+    // declare the varibales
+    const float Gravity = 9.8;
+    // delcare variable height and time
+    float height=0 , time=0;
+    // collect the falling time
+    cout << "Enter a falling time: ";
+    cin>>time;
+    // calculate the height of a free fall object
+    height = 0.5 * Gravity * time * time;
+    // print results
+    cout<<"The height of a free fall obejct at " << time << " seconds is " << height<< " meters" <<endl;
 
+    cout<<"\n----- exercise:  Simple Calculator and Logical Operations -----" << endl;
+    int a;
+    int b;
+    cout << "Enter the first number(a): ";
+    cin >> a;
+    cout << "Enter the second number(b): ";
+    cin >> b;
     return 0;
 }
 
