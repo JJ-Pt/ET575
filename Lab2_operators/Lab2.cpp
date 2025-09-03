b955349a69caebc6556008336d39ae540f2c1211
JJ-Pt <jjpenetrate@gmail.com>
2025-09-03 19:48:44 +0000

    Complete lab2

--- Lab2_operators/Lab2.cpp
+++ Lab2_operators/Lab2.cpp
@@ -55,23 +55,59 @@ int main(){
     cout << "is number1 less than number2? " << (number1 < number2) << endl;
     cout << "is number1 not equal to number2? " << (number1 != number2)<<endl;
 
+    cout<<"\n----- example 4: logical operator -----" << endl;
+    // AND operator
+    bool check_numbers = (number1 != number2) && (number1 < number2) && (number1 > 5);
+    // number1 != number2 --> true
+    // number1 < number2 --> true
+    // number1 > 5 --> false
+    // check_numbers = true && true && false = false
+    cout<<"The result of check_number is " << check_numbers<<endl;
+
+    // OR Operator
+    bool check_or = (number2 > 10) || (number2 != 3) || (number2<number1);
+    // number2 > 10 --> false
+    // number2 != 3 --> true
+    // number2 <number1 --> false
+    // false || true || false = true
+    cout <<"The result of check_number is " << check_or<<endl;
+
+    cout<<"\n----- example 5: casting ----- " << endl;
+    int n =5;
+    float m = n/2;
+    cout << "implicit casting = " << m<<endl;
 
     cout<<"\n----- exercise:  Simple Calculator and Logical Operations -----" << endl;
     int a;
     int b;
+
+    //Arithmetic Operations:
     cout << "Enter the first number(a): ";
     cin >> a;
     cout << "Enter the second number(b): ";
     cin >> b;
 
-
+    cout << "the result of a + b = " << a+b << endl;
+    cout << "the result of a - b = " << a-b << endl;
+    cout << "the result of a * b = " << a*b << endl;
+    cout << "the result of a / b = " << a/b << endl;
+    cout << "the result of a % b = " << a%b << endl;
+    
+    //Assignment Operations:
     //increase a by 10
     a += 10;
     // decreace b by 5
     b -= 5;
+    cout << "Update value of a = " << a << endl;
+    cout << "Update value of b= " << b << endl;
 
-    cout << "Update A value = " << a;
-    cout << "Update B value = " << b;
+    // Boolean operations
+    cout<<"is a greater than b? " << (a > b) << endl;
+    cout<<"is a equal to b? " << (a == b) << endl;
+    cout<<"is a not equal to b? " << (a != b) << endl;
+    cout<<"is A and B both positive? " << (a&&b > 0) << endl;
+    cout<<"is either A or B a negative number? " << (a < 0 || b < 0) << endl;
+    cout<<"Is a not greater than b? " << (!(a>b))<<endl;
 
     return 0;
 }
