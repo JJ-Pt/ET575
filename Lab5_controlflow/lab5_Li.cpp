e00f4dcf3225dfadfb69cafc982dc245b652d589
JJ-Pt <jjpenetrate@gmail.com>
2025-09-10 18:38:49 +0000

    lab5 exmaple1

--- Lab5_controlflow/lab5_Li.cpp
+++ Lab5_controlflow/lab5_Li.cpp
@@ -9,6 +9,20 @@ Sep 10, 2025
 using namespace std;
 
 int main(){
+
+    cout<<"\n ----- Example 1: boool variable ----- " << endl;
+    //check if a number is positive
+    // step 1) declare and collecr a number
+    int n;
+    cout << "Enter an number: ";
+    cin >> n;
+    // step 2) check if the number is positive
+    bool checkPositive = (n >= 0);
+    // step 3) print the result
+    cout<<"Is "<<n<<" positive? "<<checkPositive<<endl;
     
+
+
+
     return 0;
 }
