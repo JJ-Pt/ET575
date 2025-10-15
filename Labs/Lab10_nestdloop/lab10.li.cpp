85182dcfdd44fe9580005b35cc6d49f2ea414c88
JJ-Pt <jjpenetrate@gmail.com>
2025-10-15 19:24:27 +0000

    lab10

--- Labs/Lab10_nestdloop/lab10.li.cpp
+++ Labs/Lab10_nestdloop/lab10.li.cpp
@@ -63,5 +63,29 @@ int main(){
         }       
         cout<<endl;
     }
+
+    cout<<"\n-----Exercise A -----" << endl;
+    int sum=0, count=0;
+    for (int n = 20; n> 0; n-=2){
+    if (n%3 ==0){
+    count++;
+    continue;
+    }
+    sum += n;
+    }
+    cout<<"The sum is: "<<sum<<endl<<"The count is: "<<count<<endl;
+
+    cout<<"\n-----Exercise B -----" << endl;
+    int i=0, add=0;
+    while(i<=10){
+    i++;
+    if(i<5 && i !=2){
+    cout<<"i = "<<i<<endl;
+    }
+    else{continue;}
+    add +=i;
+    }
+    cout<<"Add = "<<add<<endl;
+
     return 0;
 }
