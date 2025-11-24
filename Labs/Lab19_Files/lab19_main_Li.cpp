6c82db958fd6d8e8d5c091d362bc7997e226ec3d
JJ-Pt <jjpenetrate.com>
2025-11-24 21:16:35 +0000

    lab19

---
+++ Labs/Lab19_Files/lab19_main_Li.cpp
@@ -0,0 +1,29 @@
+/*
+QiuLiangJun Li
+Nov 24, 2025
+Lab 19: Files and consle input
+*/
+
+#include <iostream>
+#include "lab19_function_Li.cpp"
+
+using namespace std;
+
+int main(){
+    cout << "\n ----- Example 1: Consle input failure -----" << endl;
+    checknumber();
+
+    cout << "\n ----- Example 2: Consle input failure in loop -----" << endl;
+    cout << "Validated number = " << vaidatenumber() << endl;
+
+    cout << "\n ----- Example 3: reading a file -----" << endl;
+    readfile("samplefile.txt");
+
+    cout << "\n ----- Example 4: writting content into a file -----" << endl;
+    writefile("outputfile.txt");
+
+    cout << "\n ----- Example 5: append data into a file -----" << endl;
+    appendfile("samplefile.txt");
+
+    return 0;
+}
